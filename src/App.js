3b0ba3ad721844ef563b119a332d238299ed75ec
tsuyoshi.hara <alra9si916323@gmail.com>
2023-09-12 22:54:36 +0900

    勝利時にマス目の色が変化するように修正

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import * as React from "react";
 import Button from "@mui/material/Button";
 import { useState } from "react";
 
-function Square({ value, onSquareClick }) {
+function Square({ value, onSquareClick, color }) {
   return (
-    <button className="square" onClick={onSquareClick}>
+    <button
+      className="square"
+      onClick={onSquareClick}
+      style={{ background: color }}
+    >
       {value}
     </button>
   );
@@ -19,11 +23,27 @@ function Board({ xIsNext, squares, onPlay, currentTurn }) {
 
     onPlay(nextSquares);
   }
+  const result = calculateWinner(squares);
+  const winner = result?.winner;
+  const lines = result?.lines;
 
-  const winner = calculateWinner(squares);
   let status;
+  let squareColor = [
+    "white",
+    "white",
+    "white",
+    "white",
+    "white",
+    "white",
+    "white",
+    "white",
+    "white",
+  ];
   if (winner) {
     status = "Winner: " + winner;
+    for (let i = 0; i < lines.length; ++i) {
+      squareColor[lines[i]] = "#0fc6e5";
+    }
   } else {
     status = "Next player: " + (xIsNext ? "X" : "O");
   }
@@ -34,19 +54,55 @@ function Board({ xIsNext, squares, onPlay, currentTurn }) {
     <React.Fragment>
       <div className="status">{status}</div>
       <div className="board-row">
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        <Square
+          value={squares[0]}
+          onSquareClick={() => handleClick(0)}
+          color={squareColor[0]}
+        />
+        <Square
+          value={squares[1]}
+          onSquareClick={() => handleClick(1)}
+          color={squareColor[1]}
+        />
+        <Square
+          value={squares[2]}
+          onSquareClick={() => handleClick(2)}
+          color={squareColor[2]}
+        />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+        <Square
+          value={squares[3]}
+          onSquareClick={() => handleClick(3)}
+          color={squareColor[3]}
+        />
+        <Square
+          value={squares[4]}
+          onSquareClick={() => handleClick(4)}
+          color={squareColor[4]}
+        />
+        <Square
+          value={squares[5]}
+          onSquareClick={() => handleClick(5)}
+          color={squareColor[5]}
+        />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        <Square
+          value={squares[6]}
+          onSquareClick={() => handleClick(6)}
+          color={squareColor[6]}
+        />
+        <Square
+          value={squares[7]}
+          onSquareClick={() => handleClick(7)}
+          color={squareColor[7]}
+        />
+        <Square
+          value={squares[8]}
+          onSquareClick={() => handleClick(8)}
+          color={squareColor[8]}
+        />
       </div>
     </React.Fragment>
   );
@@ -146,10 +202,17 @@ function calculateWinner(squares) {
     [2, 4, 6],
   ];
 
+  let result = {
+    winner: "",
+    lines: [],
+  };
+
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      result.winner = squares[a];
+      result.lines = lines[i];
+      return result;
     }
   }
   return null;
